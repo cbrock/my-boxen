e5b02c8691c7684b46fa56fd3148f7c46db4c9a9
cbrock <colintbrock@gmail.com>
2015-09-19 10:30:36 -0700

    Add Chrome Canary extensions

--- modules/people/manifests/cbrock/apps.pp
+++ modules/people/manifests/cbrock/apps.pp
@@ -1,3 +1,33 @@
 class people::cbrock::apps {
-  # TODO
+  package { 'iterm2'          : provider => 'brewcask' }
+  package { 'alfred'          : provider => 'brewcask' }
+  package { '1password'       : provider => 'brewcask' }
+  package { 'the-unarchiver'  : provider => 'brewcask' }
+  package { 'evernote'        : provider => 'brewcask' }
+
+  # brew cask versions tap
+  homebrew::tap { 'caskroom/versions': }
+  
+  package { 'sublime-text3'        : provider => 'brewcask' }
+  package { 'firefox-nightly'      : provider => 'brewcask' }
+  package { 'google-chrome-canary' : provider => 'brewcask' }
+  
+  # chrome extensions
+  chrome::canary::extension { '1Password':
+    id      => 'aomjjhallfgjeglblehebfpbcfeobpgk',
+    require => Package['google-chrome-canary']
+  }
+
+  chrome::canary::extension { 'Ember Inspector':
+    id      => 'bmdblncegkenkacieihfhpjfppoconhi',
+    require => Package['google-chrome-canary']
+  }
+
+  chrome::canary::extension { 'Adblock':
+    id      => 'gighmmpiobklfepjocnamgkkbiglidom',
+    require => Package['google-chrome-canary']
+  }
+  
+  # TODO add sublime settings, packages, possibly from copying dotfiles via:
+  # http://garylarizza.com/blog/2013/02/15/puppet-plus-github-equals-laptop-love/
 }
