580001e6f15d119ca0eac3523ecc7a44ad758d0d
cbrock <colintbrock@gmail.com>
2015-09-26 08:56:36 -0700

    Add Sublime Text 3 packages, theme, prefs

--- modules/people/manifests/cbrock/apps.pp
+++ modules/people/manifests/cbrock/apps.pp
@@ -7,11 +7,11 @@ class people::cbrock::apps {
 
   # brew cask versions tap
   homebrew::tap { 'caskroom/versions': }
-  
+
   package { 'sublime-text3'        : provider => 'brewcask' }
   package { 'firefox-nightly'      : provider => 'brewcask' }
   package { 'google-chrome-canary' : provider => 'brewcask' }
-  
+
   # chrome extensions
   chrome::canary::extension { '1Password':
     id      => 'aomjjhallfgjeglblehebfpbcfeobpgk',
@@ -27,7 +27,58 @@ class people::cbrock::apps {
     id      => 'gighmmpiobklfepjocnamgkkbiglidom',
     require => Package['google-chrome-canary']
   }
+
+  # sublime text
+  ## We're installing via brew cask, but using sublime_text_3::package to install packages
+
+  # taken from https://github.com/jozefizso/puppet-sublime_text_3/blob/master/manifests/config.pp
+  $dir = "/Users/${::boxen_user}/Library/Application Support/Sublime Text 3"
+  $packages_dir = "${dir}/Packages"
+  $user_packages_dir = "${packages_dir}/User"
+  $installed_packages_dir = "${dir}/Installed Packages"
+
+  file { [
+    $dir,
+    $packages_dir,
+    $user_packages_dir,
+    $installed_packages_dir
+  ]:
+    ensure => directory,
+    require => Package['sublime-text3']
+  }
+
+  ## settings
+  file { "${user_packages_dir}/Preferences.sublime-settings" :
+    ensure  => link,
+    mode    => '0644',
+    target  => "/Users/${::boxen_user}/dotfiles/Preferences.sublime-settings",
+    require => [Repository["/Users/${::boxen_user}/dotfiles"],
+                File[$user_packages_dir]]
+  }
+
+  ## packages
   
-  # TODO add sublime settings, packages, possibly from copying dotfiles via:
-  # http://garylarizza.com/blog/2013/02/15/puppet-plus-github-equals-laptop-love/
+  include sublime_text_3::package_control
+  
+  sublime_text_3::package { 'EditorConfig':
+    source => 'sindresorhus/editorconfig-sublime',
+    require => Package['sublime-text3']
+  }
+
+  sublime_text_3::package { 'Emmet':
+    source => 'sergeche/emmet-sublime',
+    require => Package['sublime-text3']
+  }
+
+  sublime_text_3::package { 'SublimePuppet':
+    source => 'russCloak/SublimePuppet',
+    require => Package['sublime-text3']
+  }
+
+  ## theme
+  
+  sublime_text_3::package { 'zenburn' :
+    source => 'https://github.com/colinta/zenburn',
+    require => Package['sublime-text3']
+  }
 }
