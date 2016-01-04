024e1e5e1d0da403fd913a19ff2ccf0a80c4f840
cbrock <colintbrock@gmail.com>
2016-01-03 17:07:30 -0800

    Bump nodebuild version to include node 4.2.4 and 5.3.0

--- modules/people/manifests/cbrock/node.pp
+++ modules/people/manifests/cbrock/node.pp
@@ -6,34 +6,37 @@ class people::cbrock::node {
   # nodejs::version { '0.8': }
   # nodejs::version { '0.10': }
   nodejs::version { '0.12.7': }
-  nodejs::version { '4.2.2': }
-  nodejs::version { '5.1.0': }
+  nodejs::version { '4.2.4': }
+  nodejs::version { '5.3.0': }
 
   class { 'nodejs::global':
-    version => '4.2.2'
+    version => '4.2.4'
   }
 
-  npm_module { "npm for ${nodejs::global::version}":
+  npm_module { "npm for all installed node versions":
     module       => 'npm',
     ensure       => 'present',
     node_version => '*'
   }
 
-  npm_module { "bower for ${nodejs::global::version}":
+  npm_module { "bower for all installed node versions":
     module       => 'bower',
     ensure       => 'present',
     node_version => '*'
   }
 
-  npm_module { "phantomjs for ${nodejs::global::version}":
+  npm_module { "phantomjs for installed node versions":
     module       => 'phantomjs',
     ensure       => 'present',
     node_version => '*'
   }
 
+  # installation of ember-cli for node version 5.x.x seems to fail,
+  # so only install for default node version 4.2.4
   npm_module { "ember-cli for ${nodejs::global::version}":
     module       => 'ember-cli',
     ensure       => 'present',
-    node_version => '*'
+    version      => '1.13.13',
+    node_version => "${nodejs::global::version}"
   }
 }
