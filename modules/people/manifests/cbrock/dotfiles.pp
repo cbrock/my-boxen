d285a211aceac18fb7973a2cab4d8f08e82e3928
cbrock <colin.brock@puppetlabs.com>
2016-01-15 15:41:50 -0800

    Add numberwang utility

--- modules/people/manifests/cbrock/dotfiles.pp
+++ modules/people/manifests/cbrock/dotfiles.pp
@@ -4,4 +4,17 @@ class people::cbrock::dotfiles {
   repository { "${dotfiles_dir}":
     source => 'cbrock/dotfiles',
   }
+
+  # TODO: create homebrew formula or something for the following
+
+  file { "/Users/${::boxen_user}/bin" :
+    ensure  => directory
+  }
+
+  file { "/Users/${::boxen_user}/bin/nw" :
+    ensure  => link,
+    target  => "/Users/${::boxen_user}/dotfiles/nw",
+    require => [Repository["${dotfiles_dir}"],
+                File["/Users/${::boxen_user}/bin"]]
+  }
 }
