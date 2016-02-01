98db58e611a9c354671ead41de56ee73c7b2b2d7
cbrock <colintbrock@gmail.com>
2016-01-31 22:16:35 -0800

    Add Vundle

--- modules/people/manifests/cbrock/dotfiles.pp
+++ modules/people/manifests/cbrock/dotfiles.pp
@@ -13,6 +13,25 @@ class people::cbrock::dotfiles {
     ]
   }
 
+  file { ["/Users/${::boxen_user}/.vim",
+          "/Users/${::boxen_user}/.vim/bundle"] :
+    ensure  => directory
+  }
+
+  # TODO
+  # need to install plugins, via `vim +PluginInstall +qall`, after this.
+  # wrap in `exec`?
+  #
+  # also, the following iTerm2 setting needs to be enabled:
+  # Preferences -> Profiles -> Terminal -> Terminal Emulation -> Report Terminal Type: xterm-256color
+  # maybe add iTerm2 prefs file to dotfiles?
+  repository { "/Users/${::boxen_user}/.vim/bundle/Vundle.vim":
+    source => 'VundleVim/Vundle.vim',
+    require => [
+      File["/Users/${::boxen_user}/.vim/bundle"]
+    ]
+  }
+
   # TODO: create homebrew formula or something for the following
 
   file { "/Users/${::boxen_user}/bin" :
