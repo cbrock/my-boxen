176ea877c3c92405f7317ba2ba94b6e70e932348
cbrock <colintbrock@gmail.com>
2016-01-31 17:52:11 -0800

    Add vim via homebrew

--- modules/people/manifests/cbrock/brew.pp
+++ modules/people/manifests/cbrock/brew.pp
@@ -1,4 +1,5 @@
 class people::cbrock::brew {
+  package { 'vim'      : provider => 'homebrew' }
   package { 'tree'     : provider => 'homebrew' }
   package { 'watchman' : provider => 'homebrew' }
 }
