56c74b3beb9e2cebe1a497fdc3c08d2764f8c7fa
cbrock <colin.brock@puppetlabs.com>
2016-03-14 11:25:08 -0700

    Add httpie

--- modules/people/manifests/cbrock/brew.pp
+++ modules/people/manifests/cbrock/brew.pp
@@ -2,4 +2,5 @@ class people::cbrock::brew {
   package { 'vim'      : provider => 'homebrew' }
   package { 'tree'     : provider => 'homebrew' }
   package { 'watchman' : provider => 'homebrew' }
+  package { 'httpie'   : provider => 'homebrew' }
 }
