222a6f68a1842f269fde0a39905879cf5d2b8bf1
cbrock <colintbrock@gmail.com>
2016-07-12 14:46:20 -0700

    Disable homebrew analytics

--- modules/people/manifests/cbrock/brew.pp
+++ modules/people/manifests/cbrock/brew.pp
@@ -1,4 +1,10 @@
 class people::cbrock::brew {
+  exec { 'disable_homebrew_analytics':
+    command => 'brew analytics off',
+    # ensure homebrew is installed via arbitrary homebrew package
+    require => Package['vim']
+  }
+
   package { 'vim'      : provider => 'homebrew' }
   package { 'tree'     : provider => 'homebrew' }
   package { 'watchman' : provider => 'homebrew' }
