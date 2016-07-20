aea350bbc13951f47f6f52b6515360244894778b
cbrock <colintbrock@gmail.com>
2016-07-20 11:25:21 -0700

    Add intellij-idea cask

--- modules/people/manifests/cbrock/apps.pp
+++ modules/people/manifests/cbrock/apps.pp
@@ -18,6 +18,7 @@ class people::cbrock::apps {
   package { 'hipchat'              : provider => 'brewcask' }
   package { 'charles'              : provider => 'brewcask' }
   package { 'vmware-fusion'        : provider => 'brewcask' }
+  package { 'intellij-idea'        : provider => 'brewcask' }
   # OSX El Capitan issues installing vagrant via brew cask
   # More info: https://github.com/boxen/puppet-brewcask/issues/45
   # Installing via https://github.com/boxen/puppet-vagrant in Puppetfile for now
