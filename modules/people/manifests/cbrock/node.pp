5a1e2873539599cadd0ac7d544ec11d9c0fbaccf
cbrock <colin.brock@puppet.com>
2016-10-20 15:57:07 -0700

    Update nodejs versions

--- modules/people/manifests/cbrock/node.pp
+++ modules/people/manifests/cbrock/node.pp
@@ -5,47 +5,47 @@ class people::cbrock::node {
   # nodejs::version { '0.6': }
   # nodejs::version { '0.8': }
   # nodejs::version { '0.10': }
-  nodejs::version { '0.12.7': }
-  nodejs::version { '4.4.1': }
-  nodejs::version { '5.12.0': }
+  nodejs::version { '0.12.17': }
+  nodejs::version { '4.6.1': }
+  nodejs::version { '6.9.1': }
 
   # See all available versions with `nodenv install --list'
   # If the version you need is missing, try upgrading node-build:
   # `cd /opt/boxen/node-build && git pull && cd -`
   class { 'nodejs::global':
-    version => '4.4.1'
+    version => '4.6.1'
   }
 
   npm_module { "npm for all installed node versions":
     module       => 'npm',
     ensure       => 'present',
-    node_version => '*'
+    node_version => "${nodejs::global::version}"
   }
 
   npm_module { "bower for all installed node versions":
     module       => 'bower',
     ensure       => 'present',
-    node_version => '*'
+    node_version => "${nodejs::global::version}"
   }
 
   npm_module { "phantomjs for installed node versions":
     module       => 'phantomjs-prebuilt',
     ensure       => 'present',
-    node_version => '*'
+    node_version => "${nodejs::global::version}"
   }
 
   # installation of ember-cli for node version 5.x.x seems to fail,
-  # so only install for default node version 4.2.4
+  # so only install for default global node version
   npm_module { "ember-cli for ${nodejs::global::version}":
     module       => 'ember-cli',
     ensure       => 'present',
-    version      => '2.6.2',
+    # version      => '2.6.2',
     node_version => "${nodejs::global::version}"
   }
 
   npm_module { "jshint for all installed node versions":
     module       => 'jshint',
     ensure       => 'present',
-    node_version => '*'
+    node_version => "${nodejs::global::version}"
   }
 }
